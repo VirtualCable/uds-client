f31329cfbc865aeb3e3057522a092fe9656de88a
Adolfo Gómez García <dkmaster@dkmon.com>
2024-10-10 18:54:39 +0200

    Upgraded appimage generation to use xfreerdp 3 and trixie

--- linux/installer-appimage-template.sh
+++ linux/installer-appimage-template.sh
@@ -6,8 +6,17 @@ if ! [ $(id -u) = 0 ]; then
    exit 1
 fi
 
-echo "Installing UDSClient Portable..."
+# If -r flag, remove the appimage
+if [ "$1" = "-r" ]; then
+    echo "Removing UDSClient Portable..."
+    rm /usr/bin/UDSClient-0.0.0-x86_64.AppImage
+    rm /usr/share/applications/UDSClient.desktop
+    update-desktop-database
+    echo "Removal process done."
+    exit 0
+fi
 
+echo "Installing UDSClient Portable..."
 cp UDSClient-0.0.0-x86_64.AppImage /usr/bin
 chmod 755 /usr/bin/UDSClient-0.0.0-x86_64.AppImage
 cp UDSClient.desktop /usr/share/applications
