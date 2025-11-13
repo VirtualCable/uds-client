4d11d67facade83c94038603449ea057fa428119
Adolfo Gómez <dkmaster@dkmon.com>
2025-11-13 04:14:46 +0100

    feat(mldsa-verify): Refactor signature verification to use anyhow for error handling and update dependencies

--- crates/mldsa-verify/src/lib.rs
+++ crates/mldsa-verify/src/lib.rs
@@ -1,10 +1,13 @@
+use anyhow::Result;
+
 use ml_dsa::{
     EncodedSignature, EncodedVerifyingKey, MlDsa65, Signature, VerifyingKey, signature::Verifier,
 };
 
-static PUBLIC_KEY: &[u8] = include_bytes!("../../../public-key.bin");
+// UDS Client ML DSA Public Key
+static PUBLIC_KEY: &[u8] = include_bytes!("../public-key.bin");
 
-pub fn verify_signature(message: &[u8], signature_b64: &str) -> bool {
+pub fn verify_signature(message: &[u8], signature_b64: &str) -> Result<()> {
     use base64::engine::{Engine as _, general_purpose::STANDARD};
 
     let encoded_vk = EncodedVerifyingKey::<MlDsa65>::try_from(PUBLIC_KEY).unwrap();
@@ -13,5 +16,7 @@ pub fn verify_signature(message: &[u8], signature_b64: &str) -> bool {
     let signature_enc: EncodedSignature<MlDsa65> =
         EncodedSignature::<MlDsa65>::try_from(signature_bytes.as_slice()).unwrap();
     let signature = Signature::<MlDsa65>::decode(&signature_enc).unwrap();
-    recovered_vk.verify(message, &signature).is_ok()
+    recovered_vk
+        .verify(message, &signature)
+        .map_err(|e| anyhow::anyhow!("Signature verification failed: {}", e))
 }
--- crates/shared/src/broker/api/types.rs
+++ crates/shared/src/broker/api/types.rs
@@ -32,12 +32,8 @@ use anyhow::Result;
 
 use base64::engine::{Engine as _, general_purpose::STANDARD};
 use bzip2::read::BzDecoder;
-use rsa::{RsaPublicKey, pkcs1::DecodeRsaPublicKey, pkcs1v15::Pkcs1v15Sign};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use sha2::{Digest, Sha256};
-
-use crate::consts;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Error {
@@ -71,7 +67,7 @@ pub struct Version {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Log {
     pub level: String,
-    pub ticket: String,
+    pub ticket: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -79,7 +75,8 @@ pub struct Script {
     pub script: String,
     #[serde(rename = "type")]
     pub script_type: String,
-    pub signature: String, // base64-encoded signature
+    pub signature: String,           // base64-encoded signature
+    pub signature_algorithm: String, // Optional signature algorithm
     pub params: String, // from codecs.encode(codecs.encode(json.dumps(self.parameters).encode(), 'bz2'), 'base64').decode()
     pub log: Log,
 }
@@ -111,16 +108,9 @@ impl Script {
     }
 
     pub fn verify_signature(&self) -> anyhow::Result<()> {
-        let public_key = RsaPublicKey::from_pkcs1_pem(consts::PUBLIC_KEY_PEM)?;
-
         let script = self.decoded_script()?;
-        let signature = self.decoded_signature()?;
 
-        let digest = Sha256::digest(script.as_bytes());
-
-        public_key
-            .verify(Pkcs1v15Sign::new::<Sha256>(), &digest, &signature)
-            .map_err(|e| anyhow::anyhow!("Signature verification failed: {}", e))
+        mldsa_verify::verify_signature(script.as_bytes(), &self.signature)
     }
 }
 
@@ -143,23 +133,78 @@ pub fn get_test_script() -> Script {
         .to_string(),
         script_type: "javascript".to_string(),
         signature: concat!(
-            "dMGa9458H8gpnAD+E+c92NgvLSh7INJzmoNxoaXW4BWtptc5",
-            "MeV6sO5nVjc54tEj8M8myLnKdve8jxqeAEpZ9QC/WDE/V7+U",
-            "ePgS5ASV8dWroOdI/TqiIKJbMefeBnXVwI+1dNzVYLE++AMv",
-            "lMbS4qA6vS8Efi2k8yN4ov64jOLO+aLCXJiDYKbgQgEBCDgF",
-            "f7jD9Xog+xCxV0zdUzNTj5QeBYs5aXTIaq+DPGME2LivuL3s",
-            "rd0vWv+fPztc01zM+z7PSDt+ZCorfPd84P1mzsIq9vDDgvtn",
-            "H75XrhHb3LAc/+XLQMK2lSzUJOYb3Bf4KGIurjJQujBCggT+",
-            "YauXZGkFTCc+JhKewEa85r+sAnvIaRqsRrJ2Rkt4rrQO8E79",
-            "valbQyvWKbcjH8emhpervUo0mQn4qknKTxMxOjDpL0la33G7",
-            "7DfAMlbUy8fHQyQ0pAO2lPRmJgQQTpOhVggyV/SehqXFI78P",
-            "cuuWxBSA2vZbr8GKyYdGtCJssg/R5vbScG/MlMAGd3/mWZoS",
-            "czyqxFFt1Jdu5KElTd9ihuEHc27MBgvjUFDtyjTACrPsDwge",
-            "hR4YUdrJHlca/D+9/sgLApg0an7aO8OQmAL2Dxs1dlcZzhAR",
-            "QSRAWMaLiqJPo7lP5iXq+Ua/PZW2DczGGgOW1X01dNEGtY8C",
-            "daOyozDY6EU="
+            "+6BbJUiIIWUyfFGP9RJ+kWMTqIbfQl6fxhG3+pMxy1h7lAQ6UYW13Qfyi7jgZPzn",
+            "/woEwLSGWZvxcvA16qIRWWwI3+biRtKhDG5B0ePDbRs6u4PRZsrJzaf/cdHb6xA3",
+            "2g84Rykx8OAy1h7mQPczbIBasSkIeTF5ozbCfkrJGD9XhRtMC3rfWnASugOgmaiU",
+            "4/uovb9YLZ/wdCP/YBUsnHeolzJ4PPqfZSW6CKuloWtxZEW+NhEn3oaT9tF/1JpO",
+            "1h0Dn14XIHHwsqgTdif+CPTZqWhi38OAMkZEuMrvKjiYllvmdf+3NNGk6t7Vubbg",
+            "le5FM1flviObTyyRoOqG8TyVu+duoZq2BdrxVYyrsu74QsDHXBvPQUQaC1BTHlrD",
+            "kkIk6C19TLM8NJlqA7BAJeo8qze9Z8vjxcvti2B60E6GLKWtYNdRNRR8d3VHAa1a",
+            "3fEA/C4ZsilgWnAHFDau7u95PwgxdAOMS9dKz2tH9unJ7z6gUe6OqpzhUl4c82U3",
+            "9E8kDbJI58R3pb+bv2LoPSABplOuAov9SD58f44SK5bvCqmqIVO2kIOn75A+HDgQ",
+            "E+i3EWUksT8VUoZ4hJJWJ+pNntJbJnNGHsdTVuhrvGaOSICCBq+lKM/VeEjfVFmA",
+            "ZkSk2Mi+9TiNPQ/fiXNltX4iMYWpUEGBSVM099nSuHFPJg+nDzUAIYmDXx5RfBHF",
+            "G6covqYPNUfAMXcYRs8SXnbGNElWuAokdFhx5rGW6HQnqNA2/YHY0lcsUBXovGbx",
+            "J2Y8Smq+kgUxm6LeNlCXx6sw3IbdIhwRCIdi/oI9ejSJSrCDGFwzTzGeqbzZ4wTY",
+            "twPjeLWoffl1QqsunsXTdUMnwRMVEeH4mh0xATbrm7s+/N9xCwH5JIDiRycrqgt+",
+            "33A8h060w9bQB/g/rdaTLeHzAnMtkwfA+o8wawvu+tAk8eksUWMutyh7sIYYpaL8",
+            "/NzM98vyP/LmYPhx797nzUiTv9da5Tj7o8tv1Sdo4mKtJ0L6LQbbkQqo6rAf3aQL",
+            "+zdAvlUXt/wjb42bTuaEkXT2c2tUddkmb+Q7Tj2CXAlmlC+IHijFvhXh75ueWLMk",
+            "qwtUt0ios9Axe2kMQS8vGaBvsQoUNF0+ywlfNRK2DY4B01gYtmhD/XXwSyl+p/9B",
+            "Yr0Dl5FN16VQ5KPHDFKqoMTmUr0yqbHMiOv8t9wvmYi6eUPVqYozsY4RZgQeeoEF",
+            "FniWjuDpdqHPztN4w+zCxYb+JXiVqj7yomjet1oEsNj69YPqaqsNgFnl7P98z8Ne",
+            "SkPk+Ig73KGEQgUjx6lu7XXApxS1rQR54FtI+7SZ9/eLsQFB+0alT7gJInF/6/Xq",
+            "ymbNJt86WOkd3nl7v2Y1iq+jgYxODkbe3eCSu1quufV/3HiLYILx+DP7fdiGp7SL",
+            "teflg4s5NVOG8itJAutlE6czDDczy9Rv3pj5HOTzve+8C+3d05eelCPtD1XM0wqG",
+            "hPGQXTfkjOZaaOn4zQxAfid0CUeQ1IDYcz7XaSSIEzT+8wUH0SJYzHF1CsL+ys/n",
+            "TJXW/nzBuc9d56jgwKfd8/2ei6s5LqP8D9pWkACyzWmKhOvOGTLEo8Bto6xB2NbX",
+            "X0nqaRDiZlJn6ZCL1ixEefQNNGl7LWWU5yeVmNTX5GGDyDS/APW7xiv+DW/eP7oN",
+            "XjskZkJT61QKLujQiA0bRgJDxpwAbHsyZx7ONhbMkjO92RAcKESCRWCQ8DaaLDOk",
+            "3sSKnfrhvEvApqg0lzrV5cCDmyp22zKhouEiyWm1SFYn1ut1BhhagLh4vgqmE3yI",
+            "p+CXX7nANwZaBrlAETqUSGxF5BBPqyjLaqlT3BgsTWgcR821cKcnFOeIjUj36bxZ",
+            "0jhFL2hJplalk9WKumvRlEQQVYDfo90vXO2JyapNo+L7hMPZjlxYuRPF3atpAtyJ",
+            "SPRVvKVj9HoRdV6ZAPJpROZfCujFM7qbEgETwR2PbiiBpU4D25V4hntL6CZOkN73",
+            "DHy3GrmV0YwCNW4c4rbOLMsgJKRIOnbIDDWfMzYbyqz0gB7jePImHF688Q7hzISk",
+            "XN52vjK9ZMcf651SLqY3dnoEht3UnXY0dsF2+rKSUCVEtbKYh8kvgOJUYz7QgzVJ",
+            "Y0+LHd7mo42+wSRG9czO4/dzgfQVUxc1aosUt3nkhyydEDy7DUO4ocvF3H5Sfolx",
+            "tHxWVonNum6mXTQ6tp4vaztDz1iEfJS/Ug0A5qu/ImGtSt6rSJJHC0nr5ukHzkNW",
+            "WvbSvB/PtlCmCcy97NMKLDAuqnXY0MLFMWkJuI6FHYMyRzJPuZWLoXtHWXadWZ18",
+            "Al/oTt8IJcIVxGeKVaAAP12SpE2FUGhv9wqKq1VvwNeAv7OHhL7h/fhj8ZEkDtbC",
+            "liF5ucw33VeRtUdmvPQGBv2VN+tt7sTXL3RrzEkDw98wswu6dYXOItbSbfrIx3eR",
+            "W3Bsp3LidS2/1+QGssrk/GHXQrwhUVwQrivWfQDozlbumbGO85egApPd1Gdides3",
+            "CTk5oifpfZtq3wNGJYitDvLL3ydJ+IjGZvRSDpSy8SBONrm23f8KvaOULjfJTxjx",
+            "+RaPQ2/BIxMc7NGi9NXkz8teQXeQCN7PAuWNERjafn956vDUA0Ob7Qkh5zYCJIMz",
+            "ox15DO1b/9FFNaNJKngYRtdFwPoOsIU9MTeXFKWs6lkjFAuP32h/+JtSEWjs0aSF",
+            "Qbq08nJcJ7aOFYJOy68ZUmZ/XBypUdncU2BsbyiIFpacciy/m2cGujTPdfnFNsl2",
+            "zKCDEMpmxkz+A0vvyaAf9mboBGj4CSFqT1unwMiIyVE0Aec9KQhVwtX4SEsKkF+Z",
+            "ADob21bbYQFbvvmx4zw6tjQVS5A/8Vlfb5kFMOyt1gzgMM1+YhhlHpkklDz4D6jC",
+            "Py4A4odYjUbwmbM9KE03CMmSXxSy9fcIZmCMIrAqZFJxGYzkurSr9Hh5vxDtJ2Yk",
+            "IeS2P8evzenVRwWzdI4bQZFZeKoCzyfg4aWZS/f86N3wK2/UBOEHr9SUAkMfIu+G",
+            "4QfV3zzjtK+FwCyV/qWl9DXM+A9PGNOfJ+1zTuAogpiZb+3NHirDg5MVLXQO4+3B",
+            "5OoG3RbbtvLiWUKtMpf4agtbdX0TP9c4JxBzSQsCVml6w3AD2nNCTtmMr2kV/2Oa",
+            "soDkjMxaSVrS2ybR9hKvpZIPHwDjRjE25k9NqEDHpFJMrFdRNRNblD+JxDz/mon0",
+            "t7wFBO3cMHsfW02sh4vSpd5ZZmSEXm5KZ4LLOcoA8s4FD2uEUcTNHfzMgX81coed",
+            "+NZa40hokuv/osS3QUjaaS/Uva1TsLl0lqY6QXEKXzi7Ar6Ca9uXeZMuktufgdFE",
+            "5Nr2m9XCH9SC53wGRXwzj6C6VJPDUZPjcF6FwfrmdU1sl2eD3x2SZEV6qcUMcqH+",
+            "8EPKnLjMRUVyt03v0cIIlgaXaFhhppO2drgqcgut9RW0SeQc/8nK5x6ActVch+76",
+            "bxHbVYv8UHVHhmKTbQTdT2MRglxIH3skjR0t7l2rcs6inmsCvHmf9khBbim9YTpF",
+            "piOMD2wesceYDFapU9ZaUCVAH0wUkXYj1BLjq7OCqFYXME3zE0iZcpcB+/NS57Jc",
+            "0AMKj9CrExzmjYNhb6k2eapI9Bs7VDUXnjM+1K/a/NBWOQDhIF9/zqABNDwp0Kz1",
+            "BO6qc3WS+aB2/F+r9ngF1Q2fvUIClHoAIX/jPbCuRQVBp2w+eX2o1CIwYcQL+Z+/",
+            "rbIXQbh9c7dflsdtbeFKHjaqk8VSo1LB+ZUip4B9qhQEzIJw168SEiT/gyggxCrk",
+            "omDsSQzwp1GL3i3kf7MOE8+/1unRh3zU6riC9xdPYKH3q0+WRy8rdJqObczp/mKk",
+            "J9JeY5rhKhuVPGBo3RX5wYVR7oDbo75luzXPzslbzOUNMmnbf1lsIhQGDMNEe+Sd",
+            "qJTmdfGpfY/RQJDivkMFJq5WAjLqYY6JSbh/qTk8xCoLmOhFaUbJ3AZnXkkPhdWd",
+            "9RLWcqvlEECv0a2Rr50sUe+2l7QGkGmW+9Ayae4CW4BTg/NM0FYBBslITaTSnSlZ",
+            "QFF7hofL8RcEQIIDkblEFvW4FFuapdqlRDjBcWCjGXxt1KCa/KO7DjkCdRP01r5h",
+            "czMpXkJdvGn59sDaFxXNyZ8lDSUehZiZFUcewOu5ahhFOwx/gtSAMkJ8aB9VM2R+",
+            "D2UYD3/2flDjOR1YHNLgFJ3hwOX/Jcnh2YQA8uCZ859JgjRACcCCmCTF39GtLnrW",
+            "hje+T7KWDvpYa18YgTyNI2g0E4eujxBwSgqBgNj7jdqWd3qjc+FVZNkl5V0fZYSE",
+            "nH7DkTvm0hBJ8E3r8mOtb2dnUxvmV2fM3mzb06bwh9dQY3CGrczk8honLnvV5QJB",
+            "SVp8qKnf6gqu7qkcQ3aY3gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACA4XGhsg"
         )
         .to_string(),
+        signature_algorithm: "ML DSA 65".to_string(),
         params: concat!(
             "QlpoOTFBWSZTWYztF2MAACeZgFAEOBAyAl4KIABUMiAYjQEi",
             "UzSNG1NEKUUXJMbwNrm78OGNwikUjayaRUjcESFPQlVPnSfi",
@@ -168,7 +213,7 @@ pub fn get_test_script() -> Script {
         .to_string(),
         log: Log {
             level: "debug".to_string(),
-            ticket: "dummy_ticket".to_string(),
+            ticket: Some("dummy_ticket".to_string()),
         },
     }
 }
@@ -210,5 +255,14 @@ mod tests {
         let script = get_test_script();
         script.verify_signature()?;
         Ok(())
+    }
+
+    #[test]
+    fn test_invalid_signature() {
+        let mut script = get_test_script();
+        // Corrupt the signature
+        script.signature.replace_range(32..43, "A");
+        let result = script.verify_signature();
+        assert!(result.is_err(), "Signature verification should have failed");
     }
 }
