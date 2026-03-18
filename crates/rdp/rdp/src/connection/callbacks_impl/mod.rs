8da2aa9a7deb96f66ecf11be234b62879da7b1ef
Adolfo Gómez García <dkmaster@dkmon.com>
2026-03-18 02:53:04 +0100

    simple comment fix

--- crates/rdp/rdp/src/connection/callbacks_impl/mod.rs
+++ crates/rdp/rdp/src/connection/callbacks_impl/mod.rs
@@ -26,8 +26,9 @@
 // CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+//
 // Authors: Adolfo Gómez, dkmaster at dkmon dot com
+
 mod channels;
 mod graphics; // implements graphics callbacks
 mod instance; // implements instance callbacks
