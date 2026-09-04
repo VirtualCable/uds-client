bf8450496e132eab646a72f445e7c5c6525c6c91
Janier Rodríguez <jrodriguez@virtualcable.es>
2026-09-04 16:23:39 +0200

    chore(build): rename the Windows builder image to udslauncher-builder
    Local Docker image tag only, no change to the produced binary.

--- building/windows/build.ps1
+++ building/windows/build.ps1
@@ -1,9 +1,9 @@
-# Ensure udsclient-builder is up up date
+# Ensure udslauncher-builder is up up date
 # To be executed on building/windows/ directory
-docker build -t udsclient-builder .
+docker build -t udslauncher-builder .
 # Get full path of the ../.. directory (i.e., the root of the project)
 $projectDir = Convert-Path ../..
 
 # Run the container with the current directory mounted
-docker run --rm -v ${projectDir}:c:\crate -w /crate udsclient-builder cargo build --release
+docker run --rm -v ${projectDir}:c:\crate -w /crate udslauncher-builder cargo build --release
 # Note: the target/release/launcher.exe binary will be created
